65c39f24beec7a60ff95836a3489e55f761e54c3
Rob <robe16@hotmail.co.uk>
2017-11-10 23:12:11 +0000

    Initial commit of variables

---
+++ src/resources/global_resources/variables.py
@@ -0,0 +1,27 @@
+serviceName = 'Jarvis: Config Server'
+deviceName = 'config_server'
+
+logFileName = 'config_server'
+logFileNameTimeformat = '%Y-%m-%d'
+
+logLevelUnset = 'unset'
+logLevelDebug = 'debug'
+logLevelInfo = 'info'
+logLevelWarning = 'warning'
+logLevelError = 'error'
+logLevelCritical = 'critical'
+
+logCategoryClient = 'client request'
+logCategoryProcess = 'process'
+
+timeformat = '%Y/%m/%d %H.%M.%S.%f'
+
+uri_configDevice = '/config/device/<id>'
+uri_configAccount = '/config/account/<id>'
+
+httpStatusSuccess = 200
+httpStatusBadrequest = 400
+httpStatusForbidden = 404
+httpStatusFailure = 420
+httpStatusServererror = 500
+
