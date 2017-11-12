d8437c4025054393f6f5001be3bb74ba3db981d2
Rob <robe16@hotmail.co.uk>
2017-11-12 17:01:12 +0000

    Rename of printed label

--- tests/run_test.py
+++ tests/run_test.py
@@ -1,6 +1,6 @@
 from tests.discovery import test_discovery
 
 print('********************************')
-print('Discovery tests')
+print('TESTS: Service broadcast')
 test_discovery.run_test()
 print('********************************')
